72e426faaa8b17c18ac473ee8d1934f71e347d77
eadwyne02 <adeyieddieuel@gmail.com>
2025-09-10 16:18:54 -0700

    last commit

--- todo.js
+++ todo.js
@@ -13,9 +13,10 @@ const db = firebase.firestore();
 const messaging = firebase.messaging();
 
 if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.register("todoList/firebase-messaging-sw.js")
+  navigator.serviceWorker.register("/todoList/firebase-messaging-sw.js")
     .then((registration) => {
       console.log("Service Worker registered:", registration.scope);
+      messaging.useServiceWorker(registration);
     })
     .catch((err) => console.error("Service Worker registration failed:", err));
 }
