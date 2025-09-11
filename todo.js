b32de350e7421239c232df63e5e8cc7782c72bb5
eadwyne02 <adeyieddieuel@gmail.com>
2025-09-11 02:28:58 -0700

    tired of commit

--- todo.js
+++ todo.js
@@ -14,22 +14,20 @@ const messaging = firebase.messaging();
 
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker.register("firebase-messaging-sw.js")
-    .then((registration) => {
-      console.log("Service Worker registered:", registration.scope);
-      messaging.useServiceWorker(registration);
-      getFCMToken(registration)
-    })
-    .catch((err) => console.error("Service Worker registration failed:", err));
-}
+  .then((registration) => {
+    console.log("Service Worker registered:", registration.scope);
 
-Notification.requestPermission().then((permission) => {
-  if (permission === "granted") {
-    console.log("Notification permission granted.");
-    getFCMToken();
-  } else {
-    console.log("Notification permission denied.");
-  }
-});
+    Notification.requestPermission().then((permission) => {
+      if (permission === "granted") {
+        console.log("Notification permission granted.");
+        getFCMToken(registration);   // 👈 always pass registration
+      } else {
+        console.log("Notification permission denied.");
+      }
+    });
+  })
+  .catch((err) => console.error("Service Worker registration failed:", err));
+};
 
 let userFcmToken = null;
 
