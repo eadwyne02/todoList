b3b5c1919279d6129a8b8018b1f3d97e7524042e
eadwyne02 <adeyieddieuel@gmail.com>
2025-09-11 02:13:10 -0700

    sitll tired of commiting

--- todo.js
+++ todo.js
@@ -13,7 +13,7 @@ const db = firebase.firestore();
 const messaging = firebase.messaging();
 
 if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.register("/firebase-messaging-sw.js")
+  navigator.serviceWorker.register("firebase-messaging-sw.js")
     .then((registration) => {
       console.log("Service Worker registered:", registration.scope);
       messaging.useServiceWorker(registration);
